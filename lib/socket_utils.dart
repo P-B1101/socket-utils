89d283aa0c3effd296eeb244323a1a1871280b01
PedRaM <pedramcod1101@gmail.com>
2024-10-01 18:43:52 +0330

    update some code

--- lib/socket_utils.dart
+++ lib/socket_utils.dart
@@ -1,6 +1,5 @@
 library socket_utils;
 
-export 'package:socket_utils/src/request/body_type.dart';
 export 'package:socket_utils/src/request/tcp_command.dart';
 export 'package:socket_utils/src/request/tcp_request.dart';
 export 'package:socket_utils/src/utils/constants.dart';
